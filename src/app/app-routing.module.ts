fb210ae280b5b74eae7fcd5e9541147a7ebb6a1b
farha10-tech <farha.afreen10@gmail.com>
2021-06-29 17:39:55 +0530

    Successfull

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'student', component:StudentListComponent},
+  {path:'create',component:CreateStudentComponent},
+  {path:'',redirectTo:'student',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
